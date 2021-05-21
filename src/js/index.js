fa3abc7590ea9ab0d7fd8cd72781127506aa157f
rrr <scholeer@seznam.cz>
2021-05-21 10:11:24 +0200

    Class creation

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,39 @@
 "use strict";
 
-console.log("it works!");
+class Pacman {
+  constructor(xpos, mouth) {
+    this.xpos = xpos;
+    this.mouth = mouth;
 
-let openMouth = true;
-
-document.addEventListener("click", () => {
-  const entity = document.querySelector(".entity");
-  if (openMouth) {
-    entity.style.backgroundPositionX = "85px";
-  } else {
-    entity.style.backgroundPositionX = "0px";
+    this.TILE_SIZE = 85;
   }
-  openMouth = !openMouth;
+
+  changeMouth() {
+    this.mouth = !this.mouth;
+  }
+  moveRight() {
+    this.xpos += this.TILE_SIZE;
+    this.changeMouth();
+    this.update();
+  }
+
+  update() {
+    const entity = document.querySelector(".entity");
+    
+    entity.style.left = this.xpos + "px";
+
+    if (this.mouth) {
+      entity.style.backgroundPositionX = "85px";
+    } else {
+      entity.style.backgroundPositionX = "0px";
+    }
+  }
+}
+
+const pacman = new Pacman(0, true);
+
+document.addEventListener("keydown", (event) => {
+  if (event.code === "ArrowRight") {
+    pacman.moveRight();
+  }
 });
