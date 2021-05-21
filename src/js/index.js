75f00d9322fe69b6de9ea1231b8dac4ea67ec7bd
rrr <scholeer@seznam.cz>
2021-05-21 09:50:57 +0200

    Open close mouth

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,15 @@
-'use strict';
+"use strict";
 
-console.log('it works!');
+console.log("it works!");
+
+let openMouth = true;
+
+document.addEventListener("click", () => {
+  const entity = document.querySelector(".entity");
+  if (openMouth) {
+    entity.style.backgroundPositionX = "85px";
+  } else {
+    entity.style.backgroundPositionX = "0px";
+  }
+  openMouth = !openMouth;
+});
