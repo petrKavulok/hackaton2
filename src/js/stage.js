3937534be5927013c28f2a7a7f7c758f7553b282
Hopsaheysa <stranskymarek90@gmail.com>
2021-05-21 13:04:45 +0200

    with stage changes

--- src/js/stage.js
+++ src/js/stage.js
@@ -5,26 +5,10 @@ class Stage {
     }
 
     render() {
-<<<<<<< HEAD
-        let stage = document.createElement('div')
-        stage.classList.add('stage');
-        stage.style.width = this.width;
-        stage.style.height = this.height;
-
-        return stage
-    }
-    
-    mount(parent) {
-        this.stage = this.render();
-        parent.appendChild(this.stage)
-        this.update()
-    }
-=======
       let element = document.createElement('div');
       element.innerHTML = `<div class="stage"></div>`;
       return element
   }
->>>>>>> b3ee18b221918762110ee5c2131f02af72bebe85
 
     
     mount(parent) {
@@ -33,12 +17,8 @@ class Stage {
       this.update()
   }
     update() {
-<<<<<<< HEAD
-        console.log('yo')
-=======
         const stage = this.element.querySelector(".stage");
         stage.style.width = this.width;
         stage.style.height = this.height;
->>>>>>> b3ee18b221918762110ee5c2131f02af72bebe85
     }
 }
